f8a9fbc22e4df112903ffbbf6c7b7474498c5a24
eri-akio <erikmatsumoto18@gmail.com>
2024-09-20 06:31:32 -0300

    feat: adiciona pagina carta de motivacao

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import Roadmap from './pages/Roadmap';
+import CartaDeMotivacao from './pages/CartaDeMotivacao';
 import './App.css';
 
 function App() {
   return (
     <div>
       <Navbar/>
-      <Roadmap/>
+      <CartaDeMotivacao/>
     </div>
     
   );
