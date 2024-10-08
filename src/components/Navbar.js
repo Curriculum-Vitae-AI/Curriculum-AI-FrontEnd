83d780b31a59f5df78dbf0dd58ee88ff4018a939
erik-akio <erikmatsumoto18@gmail.com>
2024-10-08 19:33:08 -0300

    feat: responsividade mobile

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,28 @@
 import styles from "../styles/components/Navbar.module.css";
 import Logo from "../assets/Logo.svg";
+import MenuFechado from "../assets/menuFechado.png";
+import MenuAberto from "../assets/menuAberto.png";
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useLocation } from 'react-router-dom';
 
 function Navbar(){
     const location = useLocation();
+    const [isResponsive, setIsResponsive] = useState(false);
+
+    const toggleResponsive = () => {
+        setIsResponsive(!isResponsive);
+      };
 
     return(
         <div className={styles.Navbar}>
-            <button className={styles.btnLogo}> 
-            <Link to="/Curriculum-AI-FrontEnd"><img className={styles.Logo} src={Logo} alt="Logo"/></Link>
-            </button>
-            <div className={styles.Botoes}>
-            <button className={`${styles.btn} ${location.pathname === '/Roadmap' ? styles.active : ''}`}>
+            <div className={`${styles.Botoes} ${isResponsive ? styles.responsive : ''}`}>   
+                <button id={styles.iconFechado} className={styles.icon} onClick={toggleResponsive}> <img src={MenuFechado} className={styles.iconFechado} alt="menu fechado"/> </button>
+                <button id={styles.iconAberto} className={styles.icon}  onClick={toggleResponsive}> <img src={MenuAberto} className={styles.iconAberto} alt="menu aberto"/> </button>
+        
+                <Link to="/Curriculum-AI-FrontEnd"><img className={styles.Logo} src={Logo} alt="Logo"/></Link>
+                
+                <button className={`${styles.btn} ${location.pathname === '/Roadmap' ? styles.active : ''}`}>
                     <Link id={styles.link} to="/Roadmap">ROADMAP</Link>
                 </button>
                 <button className={`${styles.btn} ${location.pathname === '/CartaDeMotivacao' ? styles.active : ''}`}>
